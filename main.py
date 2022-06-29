51c55e6e7d0666c96f31d5fd9aba1ef221bd158b
Gul Ozdemir <gulozdemir@guls-mbp.home>
2022-06-29 16:00:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Mad-libs
+
+
+name = str(input("Enter name "))
+locationCity = input("Which city do you live in? ")
+hobbies = str(input("What are your hobbies? "))
+degree = str(input("Enter your degree "))
+faveNum = int(input("Enter favourite number"))
+
+print("My Name is {}, I live in {}. My hobbies are {} and I studied {} as my Bachelors degree. My Favourite number is "
+      "{}".format(name, locationCity, hobbies, degree, faveNum))
